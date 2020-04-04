4ce1856c3663b3001c8ca531b00300c556da2980
ikongcms <63036588+ikongcms@users.noreply.github.com>
2020-04-04 13:44:44 +0800

    KONGCMS
    KONGCMS

---
+++ cms/app/libs/frontend/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+namespace frontend;
+
+use Api;
+
+class IndexController {
+
+    /**
+     * 首页
+     * @param  [type] $cid  [description]
+     * @param  [type] $page [description]
+     * @return [type]       [description]
+     */
+    public static function index() {
+
+        Api::render('index', array('title' => '测试接口'));
+    }
+
+}
---
+++ cms/views/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>{$title}</title>
+<script src="/static/jquery.min.js"></script>
+</head>
+<body>
+<h1>{$title}</h1>
+</body>
+</html>
